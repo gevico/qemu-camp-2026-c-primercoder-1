5f0496128d131e318b16e71b617f314660dfe5a9
primercoder <3117705952@qq.com>
2026-04-06 23:27:37 +0800

    finish 17_myfile

--- exercises/17_myfile/17_myfile.c
+++ exercises/17_myfile/17_myfile.c
@@ -9,7 +9,21 @@ void print_elf_type(uint16_t e_type) {
   const char *type_str;
   switch (e_type) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+  case ET_REL:
+    type_str = "Relocatable";
+    break;
+  case ET_EXEC:
+    type_str = "Executable";
+    break;
+  case ET_DYN:
+    type_str = "Shared Object/PIE";
+    break;
+  case ET_CORE:
+    type_str = "CORE";
+    break;
+  default:
+    type_str = "Unknown";
+    break;
   }
   printf("ELF Type: %s (0x%x)\n", type_str, e_type);
 }
@@ -24,7 +38,14 @@ int main(int argc, char *argv[]) {
   Elf64_Ehdr ehdr;
 
   // TODO: 在这里添加你的代码
-  // I AM NOT DONE
+  fd = open(filepath[0], O_RDONLY);
+  read(fd, &ehdr, sizeof(Elf64_Ehdr));
+  print_elf_type(ehdr.e_type);
+  close(fd);
+  fd = open(filepath[1], O_RDONLY);
+  read(fd, &ehdr, sizeof(Elf64_Ehdr));
+  print_elf_type(ehdr.e_type);
+  close(fd);
   
   return 0;
 }
