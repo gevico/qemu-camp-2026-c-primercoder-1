4f6e6a83a764d5295aa78e83933d52f3922eb9aa
primercoder <3117705952@qq.com>
2026-04-09 18:55:47 +0800

    finish 37_bitmap_operations

--- exercises/37_bitmap_operations/37_bitmap_operations.c
+++ exercises/37_bitmap_operations/37_bitmap_operations.c
@@ -11,13 +11,13 @@
 /* 将第 bit_index 位设置为 1（LSB 为最低位） */
 static void set_bit(unsigned char* bitmap, size_t bit_index) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    bitmap[bit_index/8] |= (1<<(bit_index%8));
 }
 
 /* 读取第 bit_index 位，返回 0/1（LSB 为最低位） */
 static int test_bit(const unsigned char* bitmap, size_t bit_index) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    return (bitmap[bit_index/8] & (unsigned char)(1<<(bit_index%8))) > 0;
 }
 
 int main(void) {
