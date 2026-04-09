3f97e6bf3f859ceba6f3c12492962a2363e9215d
primercoder <3117705952@qq.com>
2026-04-09 18:47:16 +0800

    finish 29_swap_endian

--- exercises/29_swap_endian/29_swap_endian.c
+++ exercises/29_swap_endian/29_swap_endian.c
@@ -3,7 +3,16 @@
 
 uint32_t swap_endian(uint32_t num) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    uint8_t *p1 = (uint8_t *)&num, *p2 = (uint8_t *)&num + 3;
+    uint8_t t = *p1;
+    *p1 = *p2;
+    *p2 = t;
+    p1++;
+    p2--;
+    t = *p1;
+    *p1 = *p2;
+    *p2 = t;
+    return num;
 }
 
 int main(int argc, char* argv[]) {
