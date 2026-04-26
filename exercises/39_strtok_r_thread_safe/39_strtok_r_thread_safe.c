eb2aa79b5ee1cd248c4974fec4d7e39ad43b74d1
primercoder <3117705952@qq.com>
2026-04-26 17:29:16 +0800

    finish 39_strtok_thread_safe

--- exercises/39_strtok_r_thread_safe/39_strtok_r_thread_safe.c
+++ exercises/39_strtok_r_thread_safe/39_strtok_r_thread_safe.c
@@ -8,18 +8,52 @@
  * 示例：输入 "hello,world test"，分隔符 ", " → 依次得到 hello、world、test
  */
 
-/* 判断字符 c 是否在分隔符集合 delim 中 */
 static int is_delim(char c, const char *delim) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    while (*delim) {
+        if (c == *delim) return 1;
+        delim++;
+    }
+    return 0;
 }
 
 /* 线程安全版本：通过 saveptr 维护调用状态，不使用静态变量 */
 char *strtok_r(char *str, const char *delim, char **saveptr) {
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    char *token_start;
+    
+    /* 如果是第一次调用，使用 str；否则使用 saveptr 中保存的地址 */
+    if (str != NULL) {
+        token_start = str;
+    } else {
+        token_start = *saveptr;
+    }
+    
+    /* 跳过开头的分隔符 */
+    while (*token_start && is_delim(*token_start, delim)) {
+        token_start++;
+    }
+    
+    /* 如果已经到达字符串末尾，返回 NULL */
+    if (*token_start == '\0') {
+        *saveptr = token_start;
+        return NULL;
+    }
+    
+    /* 找到当前 token 的结束位置 */
+    char *token_end = token_start;
+    while (*token_end && !is_delim(*token_end, delim)) {
+        token_end++;
+    }
+    
+    /* 如果结束位置不是字符串末尾，则将其替换为 '\0'，并更新 saveptr */
+    if (*token_end != '\0') {
+        *token_end = '\0';
+        *saveptr = token_end + 1;
+    } else {
+        *saveptr = token_end;
+    }
+    
+    return token_start;
 }
-
 int main(void) {
     char buf[] = "hello,world test";
     const char *delim = ", ";
