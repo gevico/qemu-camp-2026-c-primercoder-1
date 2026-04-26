241b4e5900b42eebda350074c0acf2c3a48ffcea
primercoder <3117705952@qq.com>
2026-04-26 14:54:01 +0800

    finish 30_debug_print

--- exercises/30_debug_print/30_debug_print.c
+++ exercises/30_debug_print/30_debug_print.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <execinfo.h>
+#include <unistd.h>
 
-#ifdef TODO
+#if defined(DEBUG_LEVEL) && (DEBUG_LEVEL == 1)
+#define DEBUG_PRINT(fmt, ...) \
+    fprintf(stdout, "DEBUG: func=%s, line=%d\n", __func__, __LINE__)
 
-#define DEBUG_PRINT(fmt, ...) 
-    // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+#elif defined(DEBUG_LEVEL) && (DEBUG_LEVEL == 2)
+#define DEBUG_PRINT(fmt, ...) \
+    fprintf(stdout, "DEBUG: func=%s, line=%d, " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
+
+#elif defined(DEBUG_LEVEL) && (DEBUG_LEVEL == 3)
+#define DEBUG_PRINT(fmt, ...) do { \
+    fprintf(stdout, "DEBUG: func=%s, line=%d, " fmt "\n", __func__, __LINE__, ##__VA_ARGS__); \
+    void *buffer[16]; \
+    int nptrs = backtrace(buffer, 16); \
+    backtrace_symbols_fd(buffer, nptrs, STDERR_FILENO); \
+} while (0)
 
 #else
-
 #define DEBUG_PRINT(fmt, ...) do {} while (0)
 
 #endif
-
-
-
-
-
-
-
-
-
-
 
 
 
